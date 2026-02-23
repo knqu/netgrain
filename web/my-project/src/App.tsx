ba11edd3838fa56d5cbfdffcc1a7da0ef26700d6
Charles Yuan <cnathany23@gmail.com>
2026-02-22 20:19:28 -0500

    Made Login and Registration Screens. Changed the App.tsx routing so you just type in the url to view the desired page. (for example www.example.com/<desired page>

--- web/my-project/src/App.tsx
+++ web/my-project/src/App.tsx
@@ -3,36 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import Account from './components/Account';
 import Leaderboard from './components/Leaderboard';
 import Simulation from './components/Simulation';
 import SimulationResults from './components/SimulationResults';
+import Login from './components/Login';
+import Registration from './components/Registration';
 
 function App() {
   return (
-    <Router>
-      <div className="">
-        <nav className="">
-          <ul className="list-none">
-            <li className="float-left p-5"><Link to="/">Home</Link></li>
-            <li className="float-left p-5"><Link to="/account">Account</Link></li>
-            <li className="float-left p-5"><Link to="/leaderboard">Leaderboard</Link></li>
-            <li className="float-left p-5"><Link to="/sim">Simulation/Generate</Link></li>
-            <li className="float-left p-5"><Link to="/sim-results">Simulation Results</Link></li>
-          </ul>
-        </nav>
-      </div>
-
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/account" element={<Account />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/sim" element={<Simulation />} />
         <Route path="/sim-results" element={<SimulationResults />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/registration' element={<Registration />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
