a44284b28cfc0ddbe6ac9c08e9b77789cedf55c3
dl52805 <luo524@purdue.edu>
2026-02-24 10:01:35 -0500

    removed crow and asio and added uwebsockets and usockets as dependencies

--- core/src/generator/main.cpp
+++ core/src/generator/main.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#include <crow.h>
-
 int main()
 {
   printf("Hello world, generator!\n");
--- core/src/simulator/main.cpp
+++ core/src/simulator/main.cpp
@@ -1,19 +1,22 @@
-#include <stdio.h>
-
 #include <fmt/base.h>
 
-#include <crow.h>
+#include <App.h>
 
 int main()
 {
   fmt::println("Hello, world!");
 
-  crow::SimpleApp app;
+  uWS::SSLApp({
+    .key_file_name = "misc/key.pem",
+    .cert_file_name = "misc/cert.pem",
+    .passphrase = "1234"
+  }).get("/*", [](auto *res, auto */*req*/) {
+    res->end("Hello world!");
+  }).listen(3000, [](auto *listen_socket) {
+    if (listen_socket) {
+      std::cout << "Listening on port " << 3000 << std::endl;
+    }
+  }).run();
 
-  CROW_ROUTE(app, "/")([]() {
-    return "Hello, world!";
-  });
-
-  app.port(8080).multithreaded().run();
+  fmt::println("Failed to listen on port 3000");
 }
-
