bbce9bb91cf5d6ca76d4d2b78421bcbee9f083b1
Collin James <james297@purdue.edu>
2026-04-02 15:34:40 -0400

    added a simulation run page

--- web/my-project/src/components/LiveChart.tsx
+++ web/my-project/src/components/LiveChart.tsx
@@ -8,23 +8,12 @@ import {
     type UTCTimestamp,
 } from "lightweight-charts";
 
-export default function ChartComponent() {
-    const initialData = [
-      { time: '2018-12-22', value: 32.51 },
-      { time: '2018-12-23', value: 31.11 },
-      { time: '2018-12-24', value: 27.02 },
-      { time: '2018-12-25', value: 27.32 },
-      { time: '2018-12-26', value: 25.17 },
-      { time: '2018-12-27', value: 28.89 },
-      { time: '2018-12-28', value: 25.46 },
-      { time: '2018-12-29', value: 23.92 },
-      { time: '2018-12-30', value: 22.68 },
-      { time: '2018-12-31', value: 22.67 },
-    ];
+interface LiveChartProps {
+  ws: WebSocket;
+}
+
+export default function ChartComponent({ws}: LiveChartProps) {
     
-    interface LiveChartProps {
-      ws: WebSocket;
-    }
 
     const LiveChart: React.FC<LiveChartProps> = ({ws}) => {
         const containerRef = useRef<HTMLDivElement | null>(null);
@@ -53,7 +42,6 @@ export default function ChartComponent() {
             });
 
 
-            candleSeries.setData(initialData);
             chart.timeScale().fitContent();
             const date = new Date(Date.UTC(2018, 12, 31, 12, 0, 0, 0));
             var value = 100;
@@ -101,11 +89,12 @@ export default function ChartComponent() {
 
     // Chart body
     function Chart() {
+        ws: WebSocket;
         return (
             <div className="Chart_outer_container">
                 <div className="Chart_inner_container">
                     <div className="Chart" >
-                        <LiveChart /* PUT PROPER WEBSOCKET HERE *//>
+                        <LiveChart ws={ws} />
                     </div>
                 </div>
             </div>
@@ -114,7 +103,6 @@ export default function ChartComponent() {
 
     return (
         <div className="ChartContainer">
-            <Chart />
             <Chart />
         </div>
     );
---
+++ web/my-project/src/components/SimulationRun.tsx
@@ -0,0 +1,29 @@
+import "../styling/SimulationRun.css"
+import React, { useEffect, useRef, useState } from 'react';
+import ChartComponent from './LiveChart';
+//import type LiveChartProps from './LiveChart';
+
+// --- Main Dashboard ---
+export default function SimulationRun() {
+
+  const [activeStock, setActiveStock] = useState('1');
+  const wsUri = "ws://localhost:5555/";
+  const websocket = new WebSocket(wsUri);
+  const wsUri2 = "ws://localhost:5556/";
+  const websocket2 = new WebSocket(wsUri2);
+  return (
+    <div className="chart-wrapper">
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={() => setActiveStock('1')}>Stock 1</button>
+        <button onClick={() => setActiveStock('2')}>Stock 2</button>
+      </div>
+      <div className={activeStock === '1' ? 'show-chart' : 'hide-chart'}>
+        <ChartComponent ws={websocket} />
+      </div>
+      <div className={activeStock === '2' ? 'show-chart' : 'hide-chart'}>
+        <ChartComponent ws={websocket2}/>
+      </div>
+      
+    </div>
+  );
+}
