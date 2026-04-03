5681168812fd55ba8deab782933bfe70923c0416
Collin James <james297@purdue.edu>
2026-04-03 03:33:05 -0400

    finished adding an incredibly simple metrics tracking system

---
+++ web/my-project/src/components/EndSimulation.tsx
@@ -0,0 +1,37 @@
+
+export interface WidgetInterface {
+  lowVal: number | null;
+  highVal: number | null;
+}
+
+export interface WidgetListProps {
+  items: WidgetInterface[];
+}
+
+
+
+export default function EndSimulation({items}: WidgetListProps) {
+  console.log(items);
+  function ParseWidget({lowVal, highVal} : WidgetInterface) {
+    
+    const lowString = lowVal !== null ? `lowVal: ${lowVal}` : "lowVal: not tracked";
+    const highString = highVal !== null ? `highVal: ${highVal}` : "highVal: not tracked";
+    return (
+      <p>{lowString}   {highString}</p>
+    );
+  }
+  const widgetList = items.map((item, index) => (
+  <li key={index}> 
+    <ParseWidget lowVal={item.lowVal} highVal={item.highVal}/> 
+  </li>
+));
+  
+  return (
+    <div>
+      <h1>result page</h1>
+      <ul>
+      {widgetList}
+      </ul>
+    </div>
+  );
+}
--- web/my-project/src/components/SimulationRun.tsx
+++ web/my-project/src/components/SimulationRun.tsx
@@ -6,6 +6,9 @@ import {
   type IChartApi,
   type UTCTimestamp,
 } from "lightweight-charts"
+
+import { type WidgetInterface, type WidgetListProps } from "./EndSimulation"
+import EndSimulation from "./EndSimulation"
 //import type LiveChartProps from './LiveChart';
 
 
@@ -30,6 +33,13 @@ var data1: pointData[] = [];
 var data2: pointData[] = [];
 var lowerBound: number;
 var upperBound: number;
+
+var data1High = 0.0;
+var data1Low = 1000.0;
+
+var data2High = 0.0;
+var data2Low = 1000.0;
+
 // --- Main Dashboard ---
 export default function SimulationRun() {
   const [activeStock, setActiveStock] = useState('1');
@@ -44,10 +54,13 @@ export default function SimulationRun() {
 
 
   var areaSeries: any;
+  const [currentPage, setPage] = useState<string>("Run");
+  const [Stats, setWidget] = useState<any>();
 
   const SimRun: React.FC = () => {
     const containerRef = useRef<HTMLDivElement | null>(null);
 
+    
     useEffect(() => {
       if (!containerRef.current) return;
       const chart: IChartApi = createChart(containerRef.current, {
@@ -73,9 +86,18 @@ export default function SimulationRun() {
         const time: UTCTimestamp = date1.getTime() / 1000 as UTCTimestamp;
         counter++;
         data1.push({ time: time, value: Number(e.data) });
+
         if (activeStock === '1') {
           areaSeries.update({ time: time, value: Number(e.data) });
         }
+
+        if (data1.at(-1)!.value > data1High) {
+          data1High = data1.at(-1)!.value;
+        }
+        if (data1.at(-1)!.value < data1Low) {
+          data1Low = data1.at(-1)!.value;
+        }
+
         if (data1.at(-1)!.value < lowerBound || data1.at(-1)!.value > upperBound) {
           lowerBound = Number.MIN_VALUE;
           upperBound = Number.MAX_VALUE;
@@ -83,9 +105,9 @@ export default function SimulationRun() {
           socketRef2.current!.send("pause");
         }
       });
-      
 
-      
+
+
       socketRef2.current!.addEventListener("open", () => {
         console.log("CONNECTED ON 2");
         console.log(`SENT: ping: ${counter2}`);
@@ -100,14 +122,22 @@ export default function SimulationRun() {
         if (activeStock === '2') {
           areaSeries.update({ time: time, value: Number(e.data) });
         }
-        if (data1.at(-1)!.value < lowerBound || data1.at(-1)!.value > upperBound) {
+
+        if (data2.at(-1)!.value > data2High) {
+          data2High = data2.at(-1)!.value;
+        }
+        if (data2.at(-1)!.value < data2Low) {
+          data2Low = data2.at(-1)!.value;
+        }
+
+        if (data2.at(-1)!.value < lowerBound || data2.at(-1)!.value > upperBound) {
           lowerBound = Number.MIN_VALUE;
           upperBound = Number.MAX_VALUE;
           socketRef1.current!.send("pause");
           socketRef2.current!.send("pause");
         }
       });
-    
+
 
       chart.timeScale().fitContent();
 
@@ -123,6 +153,8 @@ export default function SimulationRun() {
         chart.remove();
       };
     }, []);
+
+
 
     return <div className="areaChart" ref={containerRef} />
   };
@@ -143,15 +175,15 @@ export default function SimulationRun() {
 
 
   function updateChart(str: String) {
-    switch(str) {
+    switch (str) {
       case '1':
         setActiveStock('1');
         areaSeries.setData(data1);
         break;
       case '2':
-         setActiveStock('2');
-         areaSeries.setData(data2);
-         break;
+        setActiveStock('2');
+        areaSeries.setData(data2);
+        break;
     }
   };
 
@@ -189,31 +221,109 @@ export default function SimulationRun() {
 
   async function sleepOnCondition() {
     console.log("Sleep after condition demo");
-    
+
     lowerBound = Number((document.getElementById("lower bound") as HTMLInputElement).value);
     upperBound = Number((document.getElementById("upper bound") as HTMLInputElement).value);
 
   }
+
+
+  const [selectedOption, setSelectedOption] = useState("1");
+
+  function WidgetForm() {
+
+    const handleChange = (event: any) => {
+      setSelectedOption(event.target.value);
+    };
+
+  
+    return (
+      <div className="widgetData">
+        <form id="widgetData">
+          <label> <input type="radio" name="widgetVal" value="1" checked={selectedOption === "1"} onChange={handleChange} /> Default </label>
+          <label> <input type="radio" name="widgetVal" value="2" checked={selectedOption === "2"} onChange={handleChange} /> Stock 1 metrics </label>
+          <label> <input type="radio" name="widgetVal" value="3" checked={selectedOption === "3"} onChange={handleChange} /> Stock 2 metrics </label>
+          <label> <input type="radio" name="widgetVal" value="4" checked={selectedOption === "4"} onChange={handleChange} /> High Vals </label>
+          <label> <input type="radio" name="widgetVal" value="5" checked={selectedOption === "5"} onChange={handleChange} /> Low Vals </label>
+        </form>
+      </div>
+    )
+  }
+
+  
+
+  function endSim() {
+    socketRef1.current!.send("stop");
+    socketRef2.current!.send("stop");
+    var widgetVal: WidgetInterface[] = [];
+    widgetVal.length = 0;
+    switch (selectedOption) {
+      case ('1'):
+        widgetVal.push({ lowVal: data1Low, highVal: data1High })
+        widgetVal.push({ lowVal: data2Low, highVal: data2High })
+        break;
+      case ('2'):
+        widgetVal.push({ lowVal: data1Low, highVal: data1High })
+        widgetVal.push({ lowVal: null, highVal: null })
+        break;
+      case ('3'):
+        widgetVal.push({ lowVal: null, highVal: null })
+        widgetVal.push({ lowVal: data2Low, highVal: data2High })
+        break;
+      case ('4'):
+        widgetVal.push({ lowVal: null, highVal: data1High })
+        widgetVal.push({ lowVal: null, highVal: data2High })
+        break;
+      case ('5'):
+        widgetVal.push({ lowVal: data1Low, highVal: null })
+        widgetVal.push({ lowVal: data2Low, highVal: null });
+        break;
+    }
+    console.log(widgetVal);
+    setWidget(widgetVal);
+    setPage("End");
+    // determine what data is saved from the simulation for the widget
+  };
+
+  function RenderPage() {
+      console.log(Stats);
+      switch (currentPage) {
+        case "Run":
+          return (
+            <div className="chart-wrapper">
+              <button onClick={() => pause()}>Pause</button>
+              <button onClick={() => resume()}>Resume</button>
+              <button onClick={() => modify()}>Modify Demo</button>
+              <button onClick={() => sleepAfterTime()}>wait pause demo</button>
+              <button onClick={() => sleepOnCondition()}>Conditional pause demo</button>
+              <input id="sleep timer" inputMode="decimal"></input>
+              <div>
+                <input id="lower bound" inputMode="decimal"></input>
+                <input id="upper bound" inputMode="decimal"></input>
+              </div>
+              <div>
+                <button onClick={() => endSim()}>End simulation</button>
+              </div>
+              <div>
+                <WidgetForm />
+              </div>
+              <div style={{ marginBottom: '10px' }}>
+                <button onClick={() => updateChart('1')}>Stock 1</button>
+                <button onClick={() => updateChart('2')}>Stock 2</button>
+              </div>
+              <Chart />
+
+            </div>
+          );
+        case "End":
+          return (< EndSimulation items={Stats}/>);
+      }
+    }
+
   //const websocket2 = useMemo(() => new WebSocket("ws://localhost:5555/"), []);
   return (
-    <div className="chart-wrapper">
-        <button onClick={() => pause()}>Pause</button>
-        <button onClick={() => resume()}>Resume</button>
-        <button onClick={() => modify()}>Modify Demo</button>
-        <button onClick={() => sleepAfterTime()}>wait pause demo</button>
-        <button onClick={() => sleepOnCondition()}>Conditional pause demo</button>
-        <input id="sleep timer" inputMode="decimal"></input>
-        <div>
-          <input id="lower bound" inputMode="decimal"></input>
-          <input id="upper bound" inputMode="decimal"></input>
-        </div>
-      <div style={{ marginBottom: '10px' }}>
-        <button onClick={() => updateChart('1')}>Stock 1</button>
-        <button onClick={() => updateChart('2')}>Stock 2</button>
-      </div>
-      <Chart />
-
-
-    </div>
+    <>
+      <RenderPage />
+    </>
   );
 }
